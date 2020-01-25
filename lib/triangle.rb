3cba8e2d88797a2d31328f112579f75c1d702d53
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-25 20:41:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
       if @side2 == @side3
         return :isosceles
       end
-      if @side1 != @side2 != @side3
+      if @side1 != @side2 && @side2 != @side3 && @side1 != @side3
         return :scalene
       end
     end
