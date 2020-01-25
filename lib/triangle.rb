0155a5d067f22f574c158903880ecec16ae99b5e
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-25 20:57:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,6 +9,13 @@ class Triangle
   end
   
   def kind
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    end
       if @side1 == @side2 && @side1 == @side3 
         return :equilateral
       end
@@ -23,14 +30,8 @@ class Triangle
       end
       if @side1 != @side2 && @side2 != @side3 && @side1 != @side3
         return :scalene
-      else
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
       end
     end
-  end
   
   class TriangleError < StandardError
     def message
