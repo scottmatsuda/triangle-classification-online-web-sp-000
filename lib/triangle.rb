5961599cfbe0eb08eca3a22a74c288e38ef0e4c6
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-25 20:51:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
     @side3 = side3
   end
   
-  def kind(@side1, @side2, @side3)
+  def kind
     if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
       begin
         raise TriangleError
