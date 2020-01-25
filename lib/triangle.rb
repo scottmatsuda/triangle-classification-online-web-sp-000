96af0ed1902aa333a9a4ca157a0eb4d136fb7e82
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-25 20:42:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,13 @@ class Triangle
       if @side1 == @side2 && @side1 == @side3 
         return :equilateral
       end
+      if @side1 == @side2 && @side1 != @side3
+        return :isosceles
+      end
       if @side2 == @side3 && @side2 != @side1
+        return :isosceles
+      end
+      if @side3 == @side1 && @side3 != @side2
         return :isosceles
       end
       if @side1 != @side2 && @side2 != @side3 && @side1 != @side3
