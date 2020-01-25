bb3a28bdbe8f110d9eba526a7b0f858bd86d187c
Scott Matsuda <smatsuda2014@gmail.com>
2020-01-25 21:00:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   end
   
   def kind
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
         raise TriangleError
     end
       if @side1 == @side2 && @side1 == @side3 
